452a444f26747303a39b9ee835e780bac65c7ac3
brianetaveras <taveras574@gmail.com>
2020-09-06 20:35:44 -0400

    Completed MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,26 +2,34 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
-
+        if smallest_index is not i:
+            arr[smallest_index], arr[i] = arr[i], arr[smallest_index]\
+    
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    is_sorted = False
 
-
+    while not is_sorted:
+        is_sorted = True
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                is_sorted = False
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                
     return arr
-
 '''
 STRETCH: implement the Counting Sort function below
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(0, len(arr) ):
+        if arr[i] is target:
+            return i 
     return -1   # not found
 
 
@@ -9,6 +10,21 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = len(arr) - 1
 
+    while start <= end:
+        middle_index = start + (end - end) // 2
+        middle_value = arr[middle_index]
 
+        if middle_value is target:
+            return middle_index
+
+        elif target < middle_value:
+            end = middle_index - 1
+
+        else:
+            start = middle_index + 1
     return -1  # not found
+
+
